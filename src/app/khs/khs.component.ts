74440f52c1b450e3a2b7195b17113562440553b5
KHSenevirathne <khsenevirathne@gmail.com>
2023-01-25 16:55:59 +0530

    test3

---
+++ src/app/khs/khs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'KhsComponent',
+    templateUrl: './khs.component.html',
+    styleUrls: ['./khs.component.scss']
+})
+
+export class KhsComponent{
+
+}
